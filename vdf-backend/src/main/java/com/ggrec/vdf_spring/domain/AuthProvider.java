b78513595ebac12001d2ba0e04300145b3aaabf4
Georgian Grec <georgian.grec@3pillarglobal.com>
2020-04-17 00:30:30 +0300

    Deleting old packages

--- vdf-backend/src/main/java/com/ggrec/vdf_spring/domain/AuthProvider.java
+++
@@ -1,6 +0,0 @@
-package com.ggrec.vdf_spring.domain;
-
-public enum AuthProvider {
-    local,
-    facebook
-}
--- vdf-backend/src/main/java/com/ggrec/vdf_spring/util/VDFUtils.java
+++
@@ -1,9 +0,0 @@
-package com.ggrec.vdf_spring.util;
-
-public interface VDFUtils {
-
-    static boolean isNullOrEmpty(String str) {
-        return str == null || str.isEmpty();
-    }
-
-}
